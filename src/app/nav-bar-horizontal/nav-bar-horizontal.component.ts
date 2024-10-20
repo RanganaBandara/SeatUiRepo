a257dd5fde7dbac850c9efb604bfc958dd1dd5bf
RanganaBandara <127685124+RanganaBandara@users.noreply.github.com>
2024-10-20 18:23:46 +0530

    debug errors

--- src/app/nav-bar-horizontal/nav-bar-horizontal.component.ts
+++ src/app/nav-bar-horizontal/nav-bar-horizontal.component.ts
@@ -40,9 +40,7 @@ export class NavBarHorizontalComponent {
   goToSettings() {
     console.log('Navigating to Settings...');
   }
-message(){
-  this.router.navigate(['/message']);
-}
+
   // Fetch intern profile (name and image URL) from backend API
   loadInternProfile() {
     const apiUrl = 'https://your-backend-api.com/api/intern/profile'; // Replace with actual API URL
